4f72f2d4aff87379b27156a9f9a67060fd83f077
billodell <billodell5@gmail.com>
2017-12-18 15:54:18 -0800

    iife

--- public/scripts/heroView.js
+++ public/scripts/heroView.js
@@ -1,5 +1,11 @@
-heroView.initIndexPage () => {
-   $.get('/heroes')
-   .then(data => let heroes = data.map(hero => new Hero(hero)))
-   .catch(console.error)
-}
+
+(function(module) {
+
+
+    heroView.initIndexPage () => {
+        $.get('/heroes')
+        .then(data => Hero.all = data.map(hero => new app.Hero(hero)))
+        .catch(console.error)
+        }
+    module.heroView = heroView
+})(app);
