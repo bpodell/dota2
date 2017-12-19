c1fe1c7c1f0b61d9d7e1d8c1fb3babeb533aa6ba
Kevin Miller <kevin@driftabout.com>
2017-12-19 14:07:52 -0800

    pushState ideas

--- public/scripts/heroView.js
+++ public/scripts/heroView.js
@@ -7,12 +7,15 @@ var app = app || {};
   }
 
   heroView.initIndexPage = () => {
+    history.pushState( {
+      view: 'heroes-view'
+    }, null, '/heroes');
     $.get('/heroes')
       .then(data => app.Hero.all = data.map(hero => new app.Hero(hero)))
       .then(appendHeroView)
       .catch(console.error)
-      
-    //   
+
+    //
   }
   module.heroView = heroView
 })(app);
@@ -20,6 +23,9 @@ var app = app || {};
 $(function() {
   app.heroView.initIndexPage()
   $('#hero-view').on('click', 'li', function() {
+    history.pushState( {
+      view: 'stats-view'
+    }, null, `/hero-stats/${$(this).find('h2').text()}`);
     console.log($(this).attr('data-hero-id'));
     $.get(`/stats/${$(this).attr('data-hero-id')}`)
       .then(console.log)
