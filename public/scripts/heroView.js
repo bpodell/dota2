c5b43347631936b7a4c770546cc72d73d0e17be0
billodell <billodell5@gmail.com>
2017-12-21 09:28:14 -0800

    sort buttons

--- public/scripts/heroView.js
+++ public/scripts/heroView.js
@@ -2,7 +2,7 @@ var app = app || {};
 
 (function(module) {
   let heroView = {};
-  function appendHeroView() {
+  heroView.appendHeroView = () => {
 
     app.Hero.all.forEach((hero, i) => {
       hero.arrayIndex = i
@@ -41,7 +41,7 @@ var app = app || {};
     heroData.sort((a,b) => a.name < b.name ? -1 : 1 );
     console.log(heroData)
     app.Hero.all = heroData.map(hero => new app.Hero(hero))
-    appendHeroView();
+    heroView.appendHeroView();
   }
 
 
@@ -50,6 +50,13 @@ var app = app || {};
 
 $(function() {
   app.heroView.initIndexPage()
+  $('#sort-form').on('change', function(e) {
+    let eVal = $('#sort-menu').val()
+    app.Hero.all.sort((a,b) => a[eVal] < b[eVal] ? -1 : 1 );
+    if ($('#asc-menu').val() === 'desc') app.Hero.all.reverse();
+    $('#hero-view-list').empty();
+    app.heroView.appendHeroView();
+  })
   $('#hero-view-list').on('click', 'li', function() {
     app.stats.initStatsPage(this);
     $('html').animate({ scrollTop: 0 }, 600);
