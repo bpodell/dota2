6f5c3441a4ff10de83a5cbf182079cc7677361bb
Mitchell M <mitchellm1618@gmail.com>
2017-12-21 16:16:31 -0800

    added navigation styling, event listeners to trigger image changes on pros and about pages

--- public/scripts/view.js
+++ public/scripts/view.js
@@ -1,12 +1,5 @@
 'use strict';
 
-// var $video = $('.fullscreen-bg_video');
-// var downloadingVideo = new Video();
-// downloadingVmage.onload = function () {
-//     image.src = this.src;
-// };
-// downloadingVideo.src = "video/ti7-opening-reel";
-
 $('#icon, nav li').on('click', function () {
   // console.log()
   if ($(window).width() < 781) {
@@ -15,16 +8,21 @@ $('#icon, nav li').on('click', function () {
 });
 
 $('.home-nav-item').on('click', function() {
-  // if ($(window).width() < 781) {
-  //   $('.home-nav-item').toggleClass('is-visible');
-  // }
   $('.container').hide()
   $('#hero-view').show()
   $('html').animate({scrollTop:0}, 600);
   $('.fullscreen-bg').css('background', `url(../img/allHeroesEdited.jpg) center center / cover no-repeat`);
 })
 
+$('.about-nav-item').on('click', function () {
+  $('.container').hide()
+  $('#about-me').show()
+  $('html').animate({ scrollTop: 0 }, 600);
+  $('.fullscreen-bg').css('background', `url(../img/class.jpg) center center / cover no-repeat`);
+})
+
 
 $('.pros-nav-item').on('click', function() {
   initProPage();
+  $('.fullscreen-bg').css('background', `url(../img/aegis.png) center center / cover no-repeat`);
 })
