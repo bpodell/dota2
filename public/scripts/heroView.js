1a0e68f3170c0b6b8e1d44ad2fc65dcbf34d3f82
Kevin Miller <kevin@driftabout.com>
2017-12-21 16:15:54 -0800

    working with forward and bak buttons

--- public/scripts/heroView.js
+++ public/scripts/heroView.js
@@ -2,24 +2,31 @@ var app = app || {};
 
 (function(module) {
   let heroView = {};
+
   function appendHeroView() {
 
     app.Hero.all.forEach((hero, i) => {
       hero.arrayIndex = i
       $('#hero-view-list').append(hero.toHtml())
     })
-
-
   }
 
-  heroView.setURl = (view, url) => {
+  heroView.setURl = (data, url, callback) => {
     history.pushState( {
-      view: view,
+      data: data,
+      callback: callback
+    }, null, url);
+  }
+
+  heroView.resetURl = (data, url, callback) => {
+    history.replaceState( {
+      data: data,
+      callback: callback
     }, null, url);
   }
 
   heroView.initIndexPage = () => {
-    heroView.setURl('home', '/')
+    heroView.setURl('', '/', 'initIndexPage' )
     let localHeroes = localStorage.heroes ? JSON.parse(localStorage.heroes) : [];
     if (localStorage.etag === app.etag && localHeroes.length) return setAll(localHeroes);
     $.get('/heroes')
@@ -39,21 +46,57 @@ var app = app || {};
     appendHeroView();
   }
 
-
   module.heroView = heroView
+
+  module.initFunctions = {initStatsPage: module.stats.initStatsPage, initIndexPage: ()=> $('.home-nav-item').click()};
+
 })(app);
 
 $(function() {
+
   $.get('/etags').then(etag => {
     app.etag = etag
     app.heroView.initIndexPage()
   });
 
   $('#hero-view-list').on('click', 'li', function() {
-    app.stats.initStatsPage(this);
+    //app.stats.initStatsPage(this);
+    app.stats.initStatsPage($(this).attr('data-hero-index'));
     $('html').animate({ scrollTop: 0 }, 600);
   } )
+
+  $('.home-nav-item').on('click', function() {
+    app.heroView.resetURl('', '/', 'initIndexPage' )
+    $('.container').hide()
+    $('#hero-view').show()
+    $('html').animate({scrollTop:0}, 600);
+    $('.fullscreen-bg').css('background', `url(../img/allHeroesEdited.jpg) center center / cover no-repeat`);
+  })
+
+  /*********** History ***********/
+  window.onpopstate = function (event){
+    console.log('URL:', document.location, 'State:', event.state);
+    if (event.state){
+      let fn = event.state.callback;
+      app.initFunctions[fn](event.state.data)
+    }
+  }
+
 })
+
+// /*********** History ***********/
+// window.onpopstate = function (event){
+//   console.log('URL:', document.location, 'State:', event.state, 'hash:', document.location.hash);
+//   if (event.state){
+//     let fn = event.state.callback;
+//     app.initFunctions[fn](event.state.data)
+//   }
+// }
+
+
+
+
+
 
 // heroView.initIndexPage = () => {
 //   let heroData;
